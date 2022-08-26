cafa347cbc2a0b3f573def5662bf9d024009fbee
haileypak <haileypak.me@gmail.com>
2022-08-26 15:06:41 +0900

    #10171 고양이

---
+++ STEP1_입출력과 사칙연산/src/com/psh/no13/Main.java
@@ -0,0 +1,12 @@
+package com.psh.no13;
+
+public class Main {
+	public static void main(String[] args) {
+//		System.out.println("\\    /\\");
+//		System.out.println(" )  ( ')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+		
+		System.out.print("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+	}
+}

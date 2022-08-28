439168b555a5ee6143ad4f7292807f24c0783dec
hailey-pak <haileypak.me@gmail.com>
2022-08-28 23:45:11 +0900

    #2525 오븐 시계

---
+++ STEP2_조건문/src/com/psh/no6/Main.java
@@ -0,0 +1,26 @@
+package com.psh.no6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] in = br.readLine().split(" ");
+        int h = Integer.parseInt(in[0]);
+        int m = Integer.parseInt(in[1]);
+        int time = Integer.parseInt(br.readLine());
+
+        int sum = h*60+m+time;
+
+        h = sum/60;
+        m = sum%60;
+
+        if (h>=24) h-=24;
+
+        System.out.print(h + " " + m);
+        br.close();
+    }
+}

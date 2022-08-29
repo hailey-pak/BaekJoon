2d75f241f772068b8b277002407818e0985bb702
hailey-pak <haileypak.me@gmail.com>
2022-08-29 23:32:05 +0900

    #2480 주사위 세개

---
+++ STEP2_조건문/src/com/psh/no7/Main.java
@@ -0,0 +1,33 @@
+package com.psh.no7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] in = br.readLine().split(" ");
+        int a = Integer.parseInt(in[0]);
+        int b = Integer.parseInt(in[1]);
+        int c = Integer.parseInt(in[2]);
+        int sum;
+
+        if (a==b && b==c) {
+            sum = 10000 + a*1000;
+        } else if (a==b || b==c) {
+            sum = 1000 + b*100;
+        } else if (a==c) {
+            sum = 1000 + a*100;
+        } else {
+            int max = a;
+            if (max < b) max = b;
+            if (max < c) max = c;
+            sum = max*100;
+        }
+
+        System.out.print(sum);
+        br.close();
+    }
+}

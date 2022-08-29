b6d5420ac38d5d78b6fed9db5a387e0952a63f5b
hailey-pak <haileypak.me@gmail.com>
2022-08-30 00:22:17 +0900

    #2739 구구단

---
+++ STEP3_반복문/src/com/psh/no01/Main.java
@@ -0,0 +1,14 @@
+package com.psh.no01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for (int i = 1; i < 10; i++) {
+            System.out.println(a + " * " + i + " = " + a*i);
+        }
+        sc.close();
+    }
+}

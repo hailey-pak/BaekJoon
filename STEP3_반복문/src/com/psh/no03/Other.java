c8d4ef16a04ea5e6b6882da92c5957ff4680b679
haileypak <haileypak.me@gmail.com>
2022-08-31 17:43:29 +0900

    #8393 합

---
+++ STEP3_반복문/src/com/psh/no03/Other.java
@@ -0,0 +1,10 @@
+package com.psh.no03;
+
+import java.util.Scanner;
+
+public class Other {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(n++*n/2);
+    }
+}

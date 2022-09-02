fca10afb179e11c42fc3110f9b59fd851cd87687
haileypak <haileypak.me@gmail.com>
2022-09-02 13:40:49 +0900

    #3052 나머지 다른풀이 - HashSet / Stream.distinct()

---
+++ STEP4_1차원배열/src/com/psh/no3/Other.java
@@ -0,0 +1,23 @@
+package com.psh.no3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+/*
+ *   HashSet 활용 - HashSet은 중복된 값을 저장하지 않음.
+ * */
+public class Other {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            set.add(Integer.parseInt(br.readLine())%42);
+        }
+        System.out.print(set.size());
+    }
+}
+

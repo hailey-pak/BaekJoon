7ce78812617a10ab0e423ee0701a36e3676eff25
hailey-pak <haileypak.me@gmail.com>
2022-09-03 20:59:03 +0900

    Arrays.stream() 의 max() min()사용

---
+++ STEP4_1차원배열/src/com/psh/no1/Other.java
@@ -0,0 +1,26 @@
+package com.psh.no1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+*   Arrays.stream() 의 max() min()사용
+* */
+public class Other {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        String[] str = br.readLine().split(" ");
+        int[] in = Arrays.stream(str)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int max = Arrays.stream(in).max().getAsInt();
+        int min = Arrays.stream(in).min().getAsInt();
+
+        System.out.print(min +" " +max);
+    }
+}

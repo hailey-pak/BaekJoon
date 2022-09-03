6b81aaac3bde1f75a141e7ce803388d0090ddabc
hailey-pak <haileypak.me@gmail.com>
2022-09-03 21:19:06 +0900

    STEP5_함수 / #15596 정수 N개의 합

---
+++ STEP5_함수/src/com/psh/no1/Main.java
@@ -0,0 +1,20 @@
+package com.psh.no1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        int[] a = new int[str.length];
+        for (int j = 0; j < a.length; j++) {
+            a[j] = Integer.parseInt(str[j]);
+        }
+
+        Test test = new Test();
+        System.out.println(test.sum(a));
+    }
+}
---
+++ STEP5_함수/src/com/psh/no1/Test.java
@@ -0,0 +1,11 @@
+package com.psh.no1;
+
+public class Test {
+    long sum(int[] a) {
+        long ans = 0;
+        for (int i : a) {
+            ans += i;
+        }
+        return ans;
+    }
+}

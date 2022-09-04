23652938853f92f2dc82017f39756c05ff0d23f7
hailey-pak <haileypak.me@gmail.com>
2022-09-04 21:57:45 +0900

    #1065 한수 - 각 자리 수가 등차수열을 이루는 수의 개수 찾기

---
+++ STEP5_함수/src/com/psh/no3/Main.java
@@ -0,0 +1,27 @@
+package com.psh.no3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = 0;
+        for (int i = 1; i <= x; i++) {
+            if ( chkNumber(i) ) n++;
+        }
+        System.out.print(n);
+    }
+
+    public static boolean chkNumber(int n) {
+        if (n<100) return true;
+        if (n==1000) return false;
+
+        int a = n/100%10;   //100의 자리수
+        int b = n/10%10;   //10의 자리수
+        int c = n%10;   //1의 자리수
+
+        return a - b == b - c;
+    }
+}

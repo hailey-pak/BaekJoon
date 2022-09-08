f163032865426470dd0e05b9207af960410466e7
haileypak <haileypak.me@gmail.com>
2022-09-08 13:22:05 +0900

    #10250 ACM 호텔: 호텔 방 번호의 규칙을 찾아 출력하는 문제

---
+++ STEP7_기본수학1/src/com/psh/no5/Main.java
@@ -0,0 +1,27 @@
+package com.psh.no5;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String[] in = br.readLine().split(" ");
+            int H = Integer.parseInt(in[0]);    //호텔의 층 수
+            int N = Integer.parseInt(in[2]);    //몇번째 손님인지
+
+            int Y = N%H;
+            int X = N/H +1;
+            if (Y == 0) {
+                Y = H;
+                X -= 1;
+            }
+            bw.write(String.format("%d%02d", Y, X)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}

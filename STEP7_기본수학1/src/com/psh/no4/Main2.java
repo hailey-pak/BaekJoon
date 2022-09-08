30758b62c9505127d86d7180e90a385e5fd68473
haileypak <haileypak.me@gmail.com>
2022-09-08 09:59:59 +0900

    #2869 달팽이: 1차 코드

---
+++ STEP7_기본수학1/src/com/psh/no4/Main2.java
@@ -0,0 +1,26 @@
+package com.psh.no4;
+
+import java.util.Scanner;
+/*
+*   답은 틀리지 않으나, 시간 초과로 실패
+*
+* */
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();     //낮에 올라가는 거리
+        int B = scan.nextInt();     //밤에 떨어지는 거리
+        int V = scan.nextInt();     //나무높이
+
+        int sum = 0, n = 0;
+        while (sum < V) {
+            n++;
+            sum += A;
+            if (sum >= V) {
+                break;
+            }
+            sum -= B;
+        }
+        System.out.print(n);
+    }
+}

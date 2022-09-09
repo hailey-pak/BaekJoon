9f2f256353ac88014032182a76884629886c75d6
hailey-pak <haileypak.me@gmail.com>
2022-09-09 23:32:55 +0900

    #2775

---
+++ STEP7_기본수학1/src/com/psh/no6/Main.java
@@ -0,0 +1,19 @@
+package com.psh.no6;
+
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+        }
+    }
+}

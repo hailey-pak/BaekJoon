e285d7ce5c97b2a5c7fd2dc73da9037d4c294120
hailey-pak <haileypak.me@gmail.com>
2022-09-11 22:21:27 +0900

    #1978 소수찾기

---
+++ STEP8_기본수학2/src/com/psh/no1/Main.java
@@ -0,0 +1,30 @@
+package com.psh.no1;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int k = sc.nextInt();
+            if (isPrime(k)) sum++;
+        }
+
+        System.out.print(sum);
+    }
+
+    private static boolean isPrime(int k) {
+        if (k==1) return false;
+
+        for (int i = 2; i < k; i++) {
+            if (k % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

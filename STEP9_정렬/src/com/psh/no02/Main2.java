0cd45d02cd6f2a0c826578e398e1d9b9ffcc0393
haileypak <haileypak.me@gmail.com>
2022-09-14 15:41:34 +0900

    #2751 수 정렬하기2 : 병합정렬

---
+++ STEP9_정렬/src/com/psh/no02/Main2.java
@@ -0,0 +1,25 @@
+package com.psh.no02;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        MergeSort.sort(arr);    //병합정렬
+
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

039163c64d0fbf42e23bf371b1725ced86afdaa1
hailey-pak <haileypak.me@gmail.com>
2022-09-17 22:03:32 +0900

    #2108 통계학: 산술평균/중앙값/최빈값/범위

--- STEP9_정렬/src/com/psh/no05/Main.java
+++ STEP9_정렬/src/com/psh/no05/Main.java
@@ -10,47 +10,49 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+
+        int sum = 0;
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
 
-        bw.write(getAverage(arr)+"\n");
-        bw.write(getMedian(arr)+"\n");
-        bw.write(getMode(arr)+"\n");
+        //평균
+        int avg = (int) Math.round( (double)sum / arr.length );
+        bw.write(avg + "\n");
+
+        //중앙값
+        Arrays.sort(arr);
+        int median =  arr[arr.length/2];
+        bw.write(median + "\n");
+
+        //최빈값
+        int cnt = 0, max = -1, mode = arr[0];
+        boolean chk = false;
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i] == arr[i+1]) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+
+            if (cnt > max) {
+               max = cnt;
+               mode = arr[i];
+               chk = true;
+            } else if (cnt == max && chk) {
+                mode = arr[i];
+                chk = false;
+            }
+        }
+        bw.write(mode + "\n");
+
+        //범위
+        int range = arr[n-1] - arr[0];
+        bw.write(String.valueOf(range));
 
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    //산술평균 출력 - 소수점 이하 첫째 자리에서 반올림
-    private static int getAverage(int[] arr) {
-        int sum = 0;
-        for (int i : arr) {
-            sum += i;
-        }
-        double avg = (double)sum / arr.length;
-        return (int) Math.round(avg);
-    }
-
-    //중앙값 출력
-    private static int getMedian(int[] arr) {
-        Arrays.sort(arr);
-        return arr[arr.length/2];
-    }
-
-    //최빈값 출력 - 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값 출력
-    private static int getMode(int[] arr) {
-        int max = 0;
-
-
-        return max;
-    }
-
-    //범위 출력
-    private static int getRange() {
-        int val = 0;
-
-        return val;
     }
 }

5ecff37fb5d8f56206c9ea34bd016422518aaaa8
hailey-pak <haileypak.me@gmail.com>
2022-09-17 22:27:58 +0900

    #1427 소트인사이드: 내림차순 정렬 - 오름차순 정렬 후 역으로 출력

---
+++ STEP9_정렬/src/com/psh/no06/Main2.java
@@ -0,0 +1,20 @@
+package com.psh.no06;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] s = br.readLine().split("");
+        Arrays.sort(s);
+        for (int i = s.length - 1; i >= 0; i--) {
+            bw.write(s[i]);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

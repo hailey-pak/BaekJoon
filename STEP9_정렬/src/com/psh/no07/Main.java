5f2509a0de0dc383f306f96e2a306b99736e96b3
hailey-pak <haileypak.me@gmail.com>
2022-09-18 18:35:51 +0900

    Comparator 람다식으로 변경

--- STEP9_정렬/src/com/psh/no07/Main.java
+++ STEP9_정렬/src/com/psh/no07/Main.java
@@ -2,7 +2,6 @@ package com.psh.no07;
 
 import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws IOException {

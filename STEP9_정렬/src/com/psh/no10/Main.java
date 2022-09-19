ab79aa78b0ecbfec6fd259c7cdfbf6a5942553cc
hailey-pak <haileypak.me@gmail.com>
2022-09-19 19:59:24 +0900

    #10814 나이순 정렬: 이중배열 정렬 -> Arrays.sort()

--- STEP9_정렬/src/com/psh/no10/Main.java
+++ STEP9_정렬/src/com/psh/no10/Main.java
@@ -5,10 +5,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Main {
-    /**
-     *  실패 --> 안정 정렬이 아니기 때문
-     *  Arrays.sort()의 경우 dual-pivot Quicksort 알고리즘을 사용 -> 안정성 보장 못함
-     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -20,7 +16,7 @@ public class Main {
             members[i] = br.readLine().split(" ");
         }
 
-        Arrays.sort(members, Comparator.comparing(o -> o[0]));
+        Arrays.sort(members, Comparator.comparingInt(o -> Integer.parseInt(o[0])));
 
         for (String[] mem : members) {
             bw.write(mem[0] + " " + mem[1] + "\n");

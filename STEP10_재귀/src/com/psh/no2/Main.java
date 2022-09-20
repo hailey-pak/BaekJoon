8432751cd4b9d9e3c995ff4f9a5a245971d3c33d
hailey-pak <haileypak.me@gmail.com>
2022-09-20 23:08:56 +0900

    #10870 피보나치5: 재귀함수 호출을 통해 피보나치 수 계산

---
+++ STEP10_재귀/src/com/psh/no2/Main.java
@@ -0,0 +1,18 @@
+package com.psh.no2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(fibonacci(n));
+    }
+
+    private static int fibonacci(int n) {
+        if (n >= 2) {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        } else {
+            return n;
+        }
+    }
+}

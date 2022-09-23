e1389428debc3292bbda11da06a39ee5080306c3
hailey-pak <haileypak.me@gmail.com>
2022-09-23 22:44:13 +0900

    #11729 하노이 탑 이동 순서 _ 재귀 함수 사용

--- STEP10_재귀/src/com/psh/no6/Main.java
+++ STEP10_재귀/src/com/psh/no6/Main.java
@@ -1,19 +1,38 @@
 package com.psh.no6;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
+    private static final StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
+        int k = (int) (Math.pow(2, n) - 1);
+        sb.append(k).append("\n");
+        hanoi(n, 1, 2, 3);
+        System.out.println(sb);
+    }
 
-        Stack<Integer> st1 = new Stack<>();
-        Stack<Integer> st2 = new Stack<>();
-        Stack<Integer> st3 = new Stack<>();
+    /**
+     * @param n     옮길 원판의 개수
+     * @param a     시작 지점
+     * @param b     중간 지점
+     * @param c     도착 지점
+     */
+    public static void hanoi(int n, int a, int b, int c) {
 
-        for (int i = n; i >=1 ; i--) {
-            st1.push(i);
+        if (n == 1) {
+            sb.append(a).append(" ").append(c).append("\n");
+            return;
         }
 
+        //n-1개 원판을 a->b로 이동
+        hanoi(n-1, a, c, b);
+
+        //가장 밑의 원판을 a->c로 이동
+        sb.append(a).append(" ").append(c).append("\n");
+
+        //n-1개의 원판을 b->c로 이동
+        hanoi(n-1, b, a, c);
     }
 }

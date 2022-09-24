1826ec5e445d469626daa7c866a05d2983b7c199
hailey-pak <haileypak.me@gmail.com>
2022-09-24 22:40:49 +0900

    #2231 분해합

---
+++ STEP11_브루트 포스/src/com/psh/no2/Main.java
@@ -0,0 +1,28 @@
+package com.psh.no2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+
+        System.out.print(getCons(n));
+    }
+
+    private static int getCons(int n) {
+        for (int i = 1; i < n; i++) {
+            int sum = i;
+
+            int k = i;
+            while (k != 0) {
+                sum += k % 10;
+                k =  k / 10;
+            }
+
+            if (sum == n) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}

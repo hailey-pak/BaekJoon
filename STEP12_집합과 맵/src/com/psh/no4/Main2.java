3235558b02a0bc10eccf952307dffd7b05c4462a
hailey-pak <haileypak.me@gmail.com>
2022-09-29 20:32:53 +0900

    #10816 숫자 카드 2: HashMap의 getOrDefault 이용

---
+++ STEP12_집합과 맵/src/com/psh/no4/Main2.java
@@ -0,0 +1,27 @@
+package com.psh.no4;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        HashMap<String, Integer> map = new HashMap<>();
+
+        br.readLine();
+
+        for (String s : br.readLine().split(" ")) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        br.readLine();
+
+        for (String s : br.readLine().split(" ")) {
+            bw.write(map.getOrDefault(s, 0) + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

0ca9e3934869ec34ecffdf8d1a54fedb44d5c189
hailey-pak <haileypak.me@gmail.com>
2022-10-01 21:08:32 +0900

    #1764 듣보잡 : ArrayList와 ArrayList의 sort

---
+++ STEP12_집합과 맵/src/com/psh/no5/Main.java
@@ -0,0 +1,41 @@
+package com.psh.no5;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if (set.contains(str)) {
+                list.add(str);
+            }
+        }
+
+        list.sort(Comparator.naturalOrder());
+        bw.write(list.size()+"\n");
+        for (String str : list) {
+            bw.write(str + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}

fe66ee0c7927cd95050247186c557497e4bb5281
hailey-pak <haileypak.me@gmail.com>
2022-10-02 19:35:50 +0900

    #1085 직사각형에서 탈출

---
+++ STEP13_기하 1/src/com/sh/no1/Main.java
@@ -0,0 +1,22 @@
+package com.sh.no1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        // w-x, h-y, x, y
+        System.out.print(Math.min(w-x, Math.min(h-y, Math.min(x, y))));
+        br.close();
+    }
+}

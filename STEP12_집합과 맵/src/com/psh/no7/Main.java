a9f828f0b8c2d4b8ce5d0cf0f7fa5392c2e70300
hailey-pak <haileypak.me@gmail.com>
2022-10-02 18:26:53 +0900

    #11478 서로 다른 부분 문자열의 개수

---
+++ STEP12_집합과 맵/src/com/psh/no7/Main.java
@@ -0,0 +1,18 @@
+package com.psh.no7;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String s = new Scanner(System.in).next();
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i+1; j < s.length()+1; j++) {
+                set.add(s.substring(i, j));
+            }
+        }
+        System.out.println(set.size());
+    }
+}

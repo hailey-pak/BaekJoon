27000ba013e1bc0211577137e7ae986b01719170
hailey-pak <haileypak.me@gmail.com>
2022-10-09 23:52:38 +0900

    #3053 택시 기하학

---
+++ STEP13_기하 1/src/com/sh/no5/Main.java
@@ -0,0 +1,11 @@
+package com.sh.no5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double r = new Scanner(System.in).nextDouble();
+        System.out.println(Math.PI * r * r);
+        System.out.println(2 * r * r);
+    }
+}

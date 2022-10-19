1b67c3aab3fa5dd77ae9ffc48a79435508811781
hailey-pak <haileypak.me@gmail.com>
2022-10-19 21:35:54 +0900

    #11050 이항계수1

---
+++ STEP14_정수론 및 조합론/src/com/psh/no07/Main.java
@@ -0,0 +1,25 @@
+package com.psh.no07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+
+        System.out.print(factorial(n) / (factorial(n-k) * factorial(k)));
+    }
+
+    static int factorial(int n) {
+        int ans = 1;
+        for (int i = 2; i <= n; i++) {
+            ans *= i;
+        }
+        return ans;
+    }
+}

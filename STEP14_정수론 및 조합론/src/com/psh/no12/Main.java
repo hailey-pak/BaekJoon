399274526e13ea3b61b8740e98291cbe3749005d
haileypak <haileypak.me@gmail.com>
2022-10-25 15:24:34 +0900

    #2004 조합 0의 개수

--- STEP14_정수론 및 조합론/src/com/psh/no12/Main.java
+++ STEP14_정수론 및 조합론/src/com/psh/no12/Main.java
@@ -9,7 +9,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
 
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
+        long n = Long.parseLong(str[0]);
+        long m = Long.parseLong(str[1]);
+
+        long two = getCnt(n, 2) - getCnt(n-m, 2) - getCnt(m, 2);
+        long five = getCnt(n, 5) - getCnt(n-m, 5) - getCnt(m, 5);
+
+        System.out.print(Math.min(two, five));
+        br.close();
+    }
+
+    static long getCnt(long num, long k) {
+        long cnt = 0;
+        for (long i = k; i <= num; i *= k) {
+            cnt += num / i;
+        }
+        return cnt;
     }
 }

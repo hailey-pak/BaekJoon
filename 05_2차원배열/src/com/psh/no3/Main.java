f56e9ce0d3ab1d25a353adaa4e4d30001f92d552
haileypak <haileypak.me@gmail.com>
2022-10-27 15:22:01 +0900

    #2563 색종이: 2차원 배열을 활용하여 색종이 평면을 덮는 문제

---
+++ 05_2차원배열/src/com/psh/no3/Main.java
@@ -0,0 +1,38 @@
+package com.psh.no3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *  예제 입력하면 맞는데 검사하면 틀리다고 나옴..
+ *  이론 상 맞는 것 같은데 이유 모르겠음 나중에 다시 확인 해보기
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+        int inter = 0;
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(s[0]);
+            arr[i][1] = Integer.parseInt(s[1]);
+
+            for (int j = i - 1; j >= 0; j--) {
+                int x = arr[i][0];
+                int y = arr[i][1];
+                int x2 = arr[j][0];
+                int y2 = arr[j][1];
+                if (Math.abs(x - x2) < 10 && Math.abs(y - y2) < 10) {
+                    int w = 10 - Math.abs(x - x2);
+                    int h = 10 - Math.abs(y - y2);
+                    inter += w * h;
+                }
+            }
+        }
+        System.out.print(n * 100 - inter);
+        br.close();
+    }
+}

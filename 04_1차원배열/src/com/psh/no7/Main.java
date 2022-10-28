5cd78b5d6990082e24848a36887bb5cf62c8c52d
haileypak <haileypak.me@gmail.com>
2022-10-28 12:52:03 +0900

    #10807 개수 세기: 배열을 입력받고 v를 찾는 문제

---
+++ 04_1차원배열/src/com/psh/no7/Main.java
@@ -0,0 +1,26 @@
+package com.psh.no7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        String[] str = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int a : arr) {
+            if (a==v) cnt++;
+        }
+        System.out.print(cnt);
+        br.close();
+    }
+}
---
+++ 04_1차원배열/src/com/psh/no7/Main2.java
@@ -0,0 +1,22 @@
+package com.psh.no7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[201];
+        String[] str = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(str[i]) + 100]++;
+        }
+
+        int v = Integer.parseInt(br.readLine());
+        System.out.print(arr[v+100]);
+        br.close();
+    }
+}

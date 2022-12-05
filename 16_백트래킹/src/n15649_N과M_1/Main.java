7f0f1ee393279ec39cbdf95098f5c3950630b8c3
hailey-pak <haileypak.me@gmail.com>
2022-12-05 21:26:57 +0900

    #15649 N과 M (1)

--- 16_백트래킹/src/n15649_N과M_1/Main.java
+++ 16_백트래킹/src/n15649_N과M_1/Main.java
@@ -1,53 +1,43 @@
 package n15649_N과M_1;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Main {
     static int[] arr;
     static boolean[] visited;
-    static BufferedWriter bw;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        int n = Integer.parseInt(in[0]);
+        int m = Integer.parseInt(in[1]);
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-
-        arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i+1;
-        }
-        visited = new boolean[arr.length];
-        combination(arr, visited, 0, n, m);
-
-        bw.flush();
-        bw.close();
-
+        visited = new boolean[n];
+        arr = new int[m];
+        dfs(n, m, 0);
+        System.out.println(sb);
     }
 
 
-    static void combination(int[] arr, boolean[] visited, int start, int n, int r) throws IOException {
-        if (r == 0) {
-            for (int i = 0; i < arr.length; i++) {
-                if(visited[i]) {
-                    bw.write(arr[i] + " ");
-                }
+    static void dfs(int n, int m, int depth){
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
             }
-            bw.newLine();
+            sb.append("\n");
             return;
-        } else {
-            for (int i = start; i < n; i++) {
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
-                combination(arr, visited, i+1, n, r-1);
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
                 visited[i] = false;
             }
         }
     }
-
-
-
 }

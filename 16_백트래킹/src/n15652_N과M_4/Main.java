a889d472955d5f54d95ed4ceb3654443d2120c6c
haileypak <haileypak.me@gmail.com>
2022-12-06 09:47:04 +0900

    #15652 N과 M (4) : 중복조합

---
+++ 16_백트래킹/src/n15652_N과M_4/Main.java
@@ -0,0 +1,38 @@
+package n15652_N과M_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] arr;
+    static int n;
+    static int m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] in = br.readLine().split(" ");
+        n = Integer.parseInt(in[0]);
+        m = Integer.parseInt(in[1]);
+
+        arr = new int[m];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth, int idx){
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = idx; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1, i);
+        }
+    }
+}

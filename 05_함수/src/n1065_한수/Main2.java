a76c540a03c02db3594ca509d32ae3bcded8e769
hailey-pak <haileypak.me@gmail.com>
2022-12-14 00:48:07 +0900

    함수 > 한수 브루트포스 알고리즘

--- 05_함수/src/n1065_한수/Main2.java
+++ 05_함수/src/n1065_한수/Main2.java
@@ -1,21 +1,25 @@
-package com.psh.no3;
+package n1065_한수;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int n = 0;
-        for (int i = 1; i <= x; i++) {
-            if ( chkNumber(i) ) n++;
+
+        if (x < 100) {
+            System.out.print(x);
+        } else {
+            int n = 99;
+            for (int i = 100; i <= x; i++) {
+                if ( chkNumber(i) ) n++;
+            }
+            System.out.print(n);
         }
-        System.out.print(n);
     }
 
     public static boolean chkNumber(int n) {
-        if (n<100) return true;
         if (n==1000) return false;
 
         int a = n/100%10;   //100의 자리수

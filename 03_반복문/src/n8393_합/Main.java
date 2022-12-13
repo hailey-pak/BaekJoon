ee920c4dd7d1a6d60f64ae9ac4e1c079bb9041d9
hailey-pak <haileypak.me@gmail.com>
2022-12-13 23:48:25 +0900

    반복문 > 합

--- 03_반복문/src/n8393_합/Main.java
+++ 03_반복문/src/n8393_합/Main.java
@@ -1,14 +1,10 @@
-package com.psh.no03;
+package n8393_합;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-           sum+=i;
-        }
-        System.out.print(sum);
+        System.out.print(n++*n/2);
     }
 }

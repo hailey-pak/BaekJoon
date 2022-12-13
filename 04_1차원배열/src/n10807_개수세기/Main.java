781ade02dc5c9e9e3f800d4a2009f6a1500fb474
hailey-pak <haileypak.me@gmail.com>
2022-12-14 00:16:48 +0900

    1차원 배열 > 개수 세기 : 코드 2가 조금 더 빠름

--- 04_1차원배열/src/n10807_개수세기/Main.java
+++ 04_1차원배열/src/n10807_개수세기/Main.java
@@ -1,22 +1,26 @@
-package com.psh.no7;
+package n10807_개수세기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main2 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[201];
+        int[] arr = new int[n];
         String[] str = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(str[i]) + 100]++;
+            arr[i] = Integer.parseInt(str[i]);
         }
 
         int v = Integer.parseInt(br.readLine());
-        System.out.print(arr[v+100]);
+        int cnt = 0;
+        for (int a : arr) {
+            if (a==v) cnt++;
+        }
+        System.out.print(cnt);
         br.close();
     }
 }

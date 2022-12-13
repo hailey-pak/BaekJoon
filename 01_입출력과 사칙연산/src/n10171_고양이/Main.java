fe76d85ff823107bcc00476d00f7943379eb88f3
hailey-pak <haileypak.me@gmail.com>
2022-12-13 22:48:53 +0900

    입출력과 사칙연산 > 고양이

---
+++ 01_입출력과 사칙연산/src/n10171_고양이/Main.java
@@ -0,0 +1,10 @@
+package n10171_고양이;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}

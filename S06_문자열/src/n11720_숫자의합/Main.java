dee49b5530f79e59f0d3e0d45d24683b319e64f6
hailey-pak <haileypak.me@gmail.com>
2022-12-14 23:16:39 +0900

    문자열 > 숫자의 합 : 정수를 문자열로 입력받는 문제

--- S06_문자열/src/n11720_숫자의합/Main.java
+++ S06_문자열/src/n11720_숫자의합/Main.java
@@ -1,4 +1,4 @@
-package com.psh.no02;
+package n11720_숫자의합;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,9 +7,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        br.readLine();
 
-        int sum = 0;
+        int sum = 0;    
         for(char c : br.readLine().toCharArray()) {
            // sum += c-'0';
            sum += Character.getNumericValue(c);

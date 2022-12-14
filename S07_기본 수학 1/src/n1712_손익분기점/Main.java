50e61e0db1d2ccdbb5aacf8cf0ae9ac40453ee8e
hailey-pak <haileypak.me@gmail.com>
2022-12-14 23:47:35 +0900

    기본 수학 1 > 손익분기점

---
+++ S07_기본 수학 1/src/n1712_손익분기점/Main.java
@@ -0,0 +1,26 @@
+package n1712_손익분기점;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if (b >= c) {
+            System.out.print(-1);
+        } else {
+            int n = a / (c - b) + 1;
+            System.out.print(n);
+        }
+    }
+}
+
+/*
+        c*n > a + b*n  //손익분기점
+        c*n - b*n > a
+        n(c-b) > a
+        n > a / (c-b)
+* */

ea0f3c8423f468d2a1018bfafbb499f6a6be0ede
hailey-pak <haileypak.me@gmail.com>
2022-12-19 18:24:39 +0900

    동적 계획법 1 > 파도반 수열

---
+++ S17_동적 계획법 1/src/n9461_파도반수열/Main.java
@@ -0,0 +1,31 @@
+package n9461_파도반수열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long[] arr = new long[101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        arr[1] = 1L;
+        arr[2] = 1L;
+        arr[3] = 1L;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(P(num)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static long P(int n) {
+        if (arr[n] == 0) {
+            arr[n] = P(n-2) + P(n-3);
+        }
+        return arr[n];
+    }
+}

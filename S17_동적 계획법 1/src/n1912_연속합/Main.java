d7425692af72d0eb15215f84e8ea542fa04bce78
hailey-pak <haileypak.me@gmail.com>
2022-12-19 20:13:40 +0900

    동적 계획법 1 > 연속합

---
+++ S17_동적 계획법 1/src/n1912_연속합/Main.java
@@ -0,0 +1,29 @@
+package n1912_연속합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        String[] in = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(in[i]);
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.print(max);
+    }
+}

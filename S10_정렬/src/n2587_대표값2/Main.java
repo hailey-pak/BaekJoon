5720d9c78502a9e53f0aa4adf7b900ee8bf84717
hailey-pak <haileypak.me@gmail.com>
2022-12-19 20:37:17 +0900

    정렬 > 대표값2

--- S10_정렬/src/n2587_대표값2/Main.java
+++ S10_정렬/src/n2587_대표값2/Main.java
@@ -1,11 +1,11 @@
-package com.psh.no12;
+package n2587_대표값2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main2 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 

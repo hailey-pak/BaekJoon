d701a5b52837905f153ed68fc4a4e44affea2295
hailey-pak <haileypak.me@gmail.com>
2022-12-19 20:34:13 +0900

    정렬 > 수 정렬하기

--- S10_정렬/src/n2750_수정렬하기/Main.java
+++ S10_정렬/src/n2750_수정렬하기/Main.java
@@ -1,6 +1,7 @@
-package com.psh.no01;
+package n2750_수정렬하기;
 
 import java.io.*;
+import java.util.Arrays;
 
 /*
 *   이중 for문으로 배열 앞에부터 비교해서 순서 바꾸기
@@ -17,10 +18,11 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        Arrays.sort(arr);
+
 //        BubbleSort.sort(arr);     //거품정렬
 //        SelectionSort.sort(arr);    //선택정렬
-        InsertionSort.sort(arr);    //삽입정렬
-
+//        InsertionSort.sort(arr);    //삽입정렬
 
         for (int i : arr) {
             bw.write(i + "\n");

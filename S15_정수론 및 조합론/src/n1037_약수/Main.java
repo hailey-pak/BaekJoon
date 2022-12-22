9e4129d87cbd2abdffb095692b8bad9f69671d36
hailey-pak <haileypak.me@gmail.com>
2022-12-22 23:27:09 +0900

    정수론 및 조합론 > 약수

---
+++ S15_정수론 및 조합론/src/n1037_약수/Main.java
@@ -0,0 +1,29 @@
+package n1037_약수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 2;
+        int min = 1000000;
+        for (int i : arr) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
+        System.out.print(max * min);
+        br.close();
+    }
+}
